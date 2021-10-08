47a4ab61fe4755ce0f8943ff6ac850c28b381f55
mgorgero <mgorgero@uno.edu>
2021-10-07 22:50:27 -0500

    Finished making ddl, I still need to test that it all works,and what to do with the multivalued attribute

--- Orgeron_Schema.sql
+++ Orgeron_Schema.sql
@@ -4,7 +4,7 @@ PatientName     VARCHAR(30),
 PhysicianID      NUMBER(10),
 CONSTRAINT PatientPK PRIMARY KEY (PatientNumber),
 CONSTRAINT PatientFK Foreign Key (PhysicianID)
-REFERENCES Physician(PhysicianID)
+REFERENCES Physician(IDNumber)
 );
 
 CREATE TABLE Order(
@@ -12,41 +12,54 @@ OrderDate  VARCHAR(9),
 OrderTime    VARCHAR(6),
 PatientNumber NUMBER(10),
 PhysicianID NUMBER(10),
-TestName
-TestNumber
+TestName VARCHAR(30),
+TestNumber NUMBER(10)
+CONSTRAINT OrderPK PRIMARY KEY(OrderDate,OrderTime,PatientNumber,PhysicianID,TestName,TestNumber),
+CONSTRAINT PNFK FOREIGN KEY(PatientNumber) REFERENCES Patient(PatientID),
+CONSTRAINT PIDFK FOREIGN KEY(PhysicianID) REFERENCES Physcian(IDNumber),
+CONSTRAINT TNFK FOREIGN KEY(TestName) REFERENCES Test(TestName),
+CONSTRAINT TNBRFK FOREIGN KEY(TestNumber) REFERENCES Test(TestName)
 );
 
 CREATE TABLE Result();//Skip this table for now, you may need to just put the result falue to order
 
 CREATE TABLE Bed(
-BedNumber
-RoomNumber
-Unit
-PatientNumber
-NurseNumber
+BedNumber NUMBER(10),
+RoomNumber NUMBER(10),
+Unit    NUMBER(10),
+PatientNumber NUMBER(10),
+NurseNumber NUMBER(10)
+CONSTRAINT BedPK PRIMARY KEY(BedNumber),
+CONSTRAINT PNFK FOREIGN KEY(PatientNumber) REFERENCES Patient(PatientNumber),
+CONSTRAINT NNFK FOREIGN KEY(NurseNumber) REFERENCES Nurse(IDNumber)
 );
 
 CREATE TABLE Nurse(
-IDNumber
-NurseName
-Salary
-SupervisorID
-
+IDNumber NUMBER(10),
+NurseName VARCHAR(30),
+Salary NUMBER(9),
+SupervisorID NUMBER(10)
+CONSTRAINT NursePK PRIMARY KEY(IDNumber),
+CONSTRAINT NurseFK FOREIGN KEY(SupervisorID) REFERENCES Nurse(IDNumber)
 );
 
 CREATE TABLE Physician(
-IDNumber
-PatientName
-Specialty
+IDNumber NUMBER(10),
+PatientName VARCHAR(30),
+Specialty VARCHAR(30),
+CONSTRAINT PhysicianPK PRIMARY KEY(IDNumber)
 );
 
 CREATE TABLE Test(
-TestName
-TestNumber
+TestName VARCHAR(30),
+TestNumber NUMBER(10)
+CONSTRAINT TestPK PRIMARY KEY(TestName,TestNumber)
 );
 
 CREATE TABLE Timecard(
-Date
-PhysicianID
-Hours
+Date VARCHAR(9),
+PhysicianID NUMBER(10),
+Hours NUMBER(10)
+CONSTRAINT CardPK PRIMARY KEY(Date, PhysicianID),
+CONSTRAINT CardFK FOREIGN KEY(PhysicianID)
 );
