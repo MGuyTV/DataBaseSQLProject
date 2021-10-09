4d7a9ae6d5bf5c0f17877572d2bec3b0077f2079
mgorgero <mgorgero@uno.edu>
2021-10-09 15:33:31 -0500

    The schema looks to be done, but could possibly use changes later on

--- Orgeron_Schema.sql
+++ Orgeron_Schema.sql
@@ -1,47 +1,10 @@
-CREATE TABLE Patient(
-PatientNumber   NUMBER(10),
-PatientName     VARCHAR(30),
-PhysicianID      NUMBER(10),
-CONSTRAINT PatientPK PRIMARY KEY (PatientNumber),
-CONSTRAINT PatientFK Foreign Key (PhysicianID)
-REFERENCES Physician(IDNumber)
-);
-
-CREATE TABLE Order(
-OrderDate  VARCHAR(9),
-OrderTime    VARCHAR(6),
-PatientNumber NUMBER(10),
-PhysicianID NUMBER(10),
-TestName VARCHAR(30),
-TestNumber NUMBER(10)
-CONSTRAINT OrderPK PRIMARY KEY(OrderDate,OrderTime,PatientNumber,PhysicianID,TestName,TestNumber),
-CONSTRAINT PNFK FOREIGN KEY(PatientNumber) REFERENCES Patient(PatientID),
-CONSTRAINT PIDFK FOREIGN KEY(PhysicianID) REFERENCES Physcian(IDNumber),
-CONSTRAINT TNFK FOREIGN KEY(TestName) REFERENCES Test(TestName),
-CONSTRAINT TNBRFK FOREIGN KEY(TestNumber) REFERENCES Test(TestName)
-);
-
-CREATE TABLE Result();//Skip this table for now, you may need to just put the result falue to order
-
-CREATE TABLE Bed(
-BedNumber NUMBER(10),
-RoomNumber NUMBER(10),
-Unit    NUMBER(10),
-PatientNumber NUMBER(10),
-NurseNumber NUMBER(10)
-CONSTRAINT BedPK PRIMARY KEY(BedNumber),
-CONSTRAINT PNFK FOREIGN KEY(PatientNumber) REFERENCES Patient(PatientNumber),
-CONSTRAINT NNFK FOREIGN KEY(NurseNumber) REFERENCES Nurse(IDNumber)
-);
-
-CREATE TABLE Nurse(
-IDNumber NUMBER(10),
-NurseName VARCHAR(30),
-Salary NUMBER(9),
-SupervisorID NUMBER(10)
-CONSTRAINT NursePK PRIMARY KEY(IDNumber),
-CONSTRAINT NurseFK FOREIGN KEY(SupervisorID) REFERENCES Nurse(IDNumber)
-);
+DROP TABLE TimeCard;
+DROP TABLE Bed;
+DROP TABLE Nurse;
+DROP TABLE TheOrder;
+DROP TABLE Patient;
+DROP TABLE TheTest;
+DROP TABLE Physician;
 
 CREATE TABLE Physician(
 IDNumber NUMBER(10),
@@ -50,16 +13,62 @@ Specialty VARCHAR(30),
 CONSTRAINT PhysicianPK PRIMARY KEY(IDNumber)
 );
 
-CREATE TABLE Test(
+CREATE TABLE TheTest(
 TestName VARCHAR(30),
-TestNumber NUMBER(10)
+TestNumber NUMBER(10),
 CONSTRAINT TestPK PRIMARY KEY(TestName,TestNumber)
 );
 
-CREATE TABLE Timecard(
-Date VARCHAR(9),
+CREATE TABLE Patient(
+PatientNumber   NUMBER(10),
+PatientName     VARCHAR(30),
+PhysicianID      NUMBER(10),
+CONSTRAINT PatientPK PRIMARY KEY (PatientNumber),
+CONSTRAINT PatientFK FOREIGN KEY (PhysicianID)
+REFERENCES Physician(IDNumber)
+);
+
+CREATE TABLE TheOrder(
+OrderDate  VARCHAR(9),
+OrderTime    VARCHAR(6),
+Results VARCHAR(30),
+PatientNumber NUMBER(10),
 PhysicianID NUMBER(10),
-Hours NUMBER(10)
-CONSTRAINT CardPK PRIMARY KEY(Date, PhysicianID),
-CONSTRAINT CardFK FOREIGN KEY(PhysicianID)
+TestName VARCHAR(30),
+TestNumber NUMBER(10),
+CONSTRAINT OrderPK PRIMARY KEY(OrderDate,OrderTime,Results,PatientNumber,PhysicianID,TestName,TestNumber),
+CONSTRAINT PatientNFK FOREIGN KEY(PatientNumber) REFERENCES Patient(PatientNumber),
+CONSTRAINT PhysicianIDFK FOREIGN KEY(PhysicianID) REFERENCES Physician(IDNumber),
+CONSTRAINT TestNFK FOREIGN KEY(TestName) REFERENCES TheTest(TestName),
+CONSTRAINT TestNBRFK FOREIGN KEY(TestNumber) REFERENCES TheTest(TestNumber)
+);
+
+--CREATE TABLE Result();//Skip this table for now, you may need to just put the result falue to order
+
+CREATE TABLE Nurse(
+IDNumber NUMBER(10),
+NurseName VARCHAR(30),
+Salary NUMBER(9),
+SupervisorID NUMBER(10),
+CONSTRAINT NursePK PRIMARY KEY(IDNumber),
+CONSTRAINT NurseFK FOREIGN KEY(SupervisorID) REFERENCES Nurse(IDNumber)
+);
+
+CREATE TABLE Bed(
+BedNumber NUMBER(10),
+RoomNumber NUMBER(10),
+Unit    NUMBER(10),
+PatientNumber NUMBER(10),
+NurseNumber NUMBER(10),
+CONSTRAINT BedPK PRIMARY KEY(BedNumber),
+CONSTRAINT PNFK FOREIGN KEY(PatientNumber) REFERENCES Patient(PatientNumber),
+CONSTRAINT NNFK FOREIGN KEY(NurseNumber) REFERENCES Nurse(IDNumber)
+);
+
+CREATE TABLE Timecard(
+TimeDate VARCHAR(9),
+PhysicianID NUMBER(10),
+TimeHours NUMBER(10),
+CONSTRAINT CardPK PRIMARY KEY(TimeDate, PhysicianID),
+CONSTRAINT CardFK FOREIGN KEY (PhysicianID) REFERENCES Physician(IDNumber)
 );
