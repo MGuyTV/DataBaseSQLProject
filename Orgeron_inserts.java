a6526460390eea5773aca5f9538f16595aed95d2
mgorgero <mgorgero@uno.edu>
2021-10-12 15:05:52 -0500

    modified the csv files, and I think I made something that is workable for the java file, but I think it probably still needs work

--- Orgeron_inserts.java
+++ Orgeron_inserts.java
@@ -16,24 +16,43 @@ public class Orgeron_inserts{
                                "C:\\Users\\orger\\source\\repos\\dbms\\Bed.csv",
                                "C:\\Users\\orger\\source\\repos\\dbms\\Physician.csv",
                                "C:\\Users\\orger\\source\\repos\\dbms\\Test.csv",
-                               "C:\\Users\\orger\\source\\repos\\dbms\\TimeCard.csv"};
+                               "C:\\Users\\orger\\source\\repos\\dbms\\TimeCard.csv"};//Could I use ./Nurse.csv instead for example?
         
         try{//somewheres in here, I need to output a string into an sql insertion file
             for(int i = 0; i < tablePaths.length; i++){//basically loop through each of the csv files, generating an insert statement
                 Scanner s = new Scanner(new File(tablePaths[i]));//cvs file
                 s.useDelimiter("\n");//will this work?
                 String fileName = fileName(new File(tablePaths[i]));//table name 
+                fileName = fileName.substring(0, fileName.length() -4);
                 while(s.hasNext()){
                 //get the
                     String value = s.next();
+                  //  System.out.println(value);
+                   // value = value + "\n";
+               //    System.out.println(value);
+                    //value = value.substring(0, fileName.length() -1);
+    
                     String sqlStatement = String.format("INSERT INTO %s VALUES (%s);\n", fileName, value);
+                  //  System.out.print(sqlStatement);
                     //after this, write this statement to the file
+                    File insertionSQL = new File("Orgeron_inserts.sql");
+                    FileWriter writer = new FileWriter(insertionSQL, true);
+                    writer.append(sqlStatement);
+                    writer.close();
+
                 }
+                if(s.hasNext() == false)
+                    s.close();
 
 
             }
-            //after the for loop is finished, append a COMMIT statement ot the end of the file
+           /* FileWriter w = new FileWriter("Orgeron_inserts.sql");
+            w.write("COMMIT;");
+            w.close();*/
         }catch(FileNotFoundException e){
+            e.printStackTrace();
+        }
+        catch(IOException e){
             e.printStackTrace();
         }
 
