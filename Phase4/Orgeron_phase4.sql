61d98b3da14bab7e4e8b142206fc73fbed4e2203
mgorgero <mgorgero@uno.edu>
2021-10-31 13:54:25 -0500

    Number 11 is all thats left

--- Phase4/Orgeron_phase4.sql
+++ Phase4/Orgeron_phase4.sql
@@ -48,17 +48,11 @@ FROM Physician, TimeCard
 Where Timecard.PhysicianID = Physician.IDNumber--Check
 AND Physician.Specialty = 'general practice';
 --9. List the names of all physicians who have not submitted a timecard. 
-SELECT PhysicianName
-FROM Physician, Timecard
-WHERE Timecard.PhysicianID != Physician.IDNumber
-ORDER BY PhysicianName;
 
-
-
-CREAT VIEW MYVIEW1 AS SELECT Physician.IDNumber, COUNT(Physician.IDNumber)
+SELECT Physician.IDNumber,PhysicianName, COUNT(Physician.IDNumber)
 FROM Physician ,  TimeCard
-WHERE Physician.IDNumber != Timecard.PhysicianID
-GROUP BY Physician.IDNumber
+WHERE Physician.IDNumber != Timecard.PhysicianID--check
+GROUP BY Physician.IDNumber, PhysicianName
 HAVING COUNT(Physician.IDNumber) = (SELECT COUNT(Timecard.PhysicianID) FROM Timecard);--the count of the ones it doesnt match = the count of the ones there are in the timecard row
 
 
@@ -69,41 +63,35 @@ WHERE Bed.NurseNumber = Nurse.IDNumber--check
 GROUP BY Nurse.IDNumber
 HAVING COUNT(Bed.NurseNumber) = 2;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 --11. For each nurse whose salary is greater than $70K, list the nurse’s name and the number of beds they monitor.
-SELECT NurseName, COUNT(Bed.NurseNumber)
+SELECT Nurse.IDNumber, Count(Nurse.IDNumber)
 FROM Nurse, Bed
-WHERE Bed.NurseNumber = Nurse.IDNumber;
-AND Salary > 70000
-GROUP BY Bed.NurseName;
+WHERE Salary > 70000
+AND Nurse.IDNumber = Bed.NurseNumber
+GROUP BY Nurse.IDNumber;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 --12. Retrieve the names of a nurses whose supervisor has a salary greater than $90K.
 SELECT *
 FROM Nurse N1, Nurse N2
 WHERE N1.SupervisorID = N2.IDNumber--check
 AND N2.Salary > 90000;
 --13. For each specialty, find the total number of physicians and the total hours worked by those physicians.
-SELECT Specialty, COUNT(Physician.PhysicianName), SUM(Timecard.TimeHours)
+SELECT Specialty, COUNT(Physician.IDNumber), SUM(Timecard.TimeHours)
 FROM Physician, Timecard
-WHERE Physician.IDNumber = Timecard.PhysicianID
+WHERE Physician.IDNumber = Timecard.PhysicianID--This only works if a physician submitted a timecard
 GROUP BY Specialty;
 --Retrieval Queries Requiring Regular Expressions (9 points, 3 pts each)
 --14. Find physicians whose specialty consists of exactly 2 words. For example, the query should return records for “General Practice”, but not “Oncology”.
