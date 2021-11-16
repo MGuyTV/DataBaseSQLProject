5bc4876e3b6e426ffe47135da51dd99e5bbf9a2a
mgorgero <mgorgero@uno.edu>
2021-11-16 16:57:51 -0600

    Question two is working

--- Phase5/Orgeron_Trigger.sql
+++ Phase5/Orgeron_Trigger.sql
@@ -16,10 +16,27 @@ BEGIN
 raise_application_error(-20001, 'No nurse can monitor more than 1 bed!');
 END;
 /
+
+--2. Write a trigger that will fire when a user attempts to DELETE one or more rows from BED. This trigger 
+--will update the Nurse.BedsMonitored for the corresponding Nurse’s ID by decreasing the value by one each time a row is removed from BED.
+CREATE OR REPLACE TRIGGER TriggerTwo
+BEFORE DELETE ON Bed
+FOR EACH ROW
+DECLARE
+MyNumber Integer;
+BEGIN
+MyNumber := :old.NurseNumber;
+UPDATE Nurse
+SET BedsMonitored = BedsMonitored -1
+WHERE Nurse.IDNumber = MyNumber;
+END;
+/
+
+
 INSERT INTO Patient VALUES (40, 'Gandalf', 19);
 INSERT INTO Patient VALUES (41, 'Dumbledore', 20);
 INSERT INTO Patient VALUES (42, 'Palpatine', 21);
-INSERT INTO Patient VALUES (43, 'Voldemort', 22);
+
 
 --(BedNumber, Room, Unit, NurseID)
 INSERT INTO Bed VALUES(1, 101, 'ICU', 40, 52);
@@ -31,17 +48,13 @@ SET BedsMonitored = 2 WHERE Nurse.IDNumber = 52;
 INSERT INTO Bed VALUES(3, 105, 'ICU', 42, 52);
 UPDATE Nurse
 SET BedsMonitored = 3 WHERE Nurse.IDNumber = 52;
-INSERT INTO Bed VALUES(4, 111, 'ICU', 43, 52);
-UPDATE Nurse
-SET BedsMonitored = 4 WHERE Nurse.IDNumber = 52;
+
+Delete from bed where Bed.BedNumber = 1;
 
 SELECT * FROM Nurse;
 SELECT * FROM Bed;
 
 
-
---2. Write a trigger that will fire when a user attempts to DELETE one or more rows from BED. This trigger 
---will update the Nurse.BedsMonitored for the corresponding Nurse’s ID by decreasing the value by one each time a row is removed from BED.
 
 --3. Write a trigger that will fire when the nurse ID for a bed record is changed (i.e., an UPDATE). 
 --This trigger will update the Nurse.BedsMonitored for both the previous Nurse and the new Nurse using
